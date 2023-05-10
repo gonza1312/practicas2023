ae588aa8bd3cc090a13185fab8bef49a97babaf8
gonza <gonzalogabrielroldan@gmail.com>
2023-05-10 00:07:36 -0300

    Ejercicio promedios Empiezo juego

--- src/main/java/ar/com/practicas/Main.java
+++ src/main/java/ar/com/practicas/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //MainNumeros.main(null);
         MainJuegoDeRol.main(null);
+
+
     }
 }
--- src/main/java/ar/com/practicas/juegoderol/Controller.java
+++ src/main/java/ar/com/practicas/juegoderol/Controller.java
@@ -6,5 +6,10 @@ public class Controller {
         while (continuar){
             continuar=IngresarDato.ingresarBoolean("Desea continuar?");
         }
+
+//        Crear una clase menu en la cual tenga un menu que tenga como opciones ataque, ataque esp, defender, abandonar, salir. Enumerados.
+//        Si elegis salir que salga del juego. Crear otro metodo que se llame decidir que sepa resolver la elección
+//        Crear una clase que se llame menu inicial en donde se pueda cargar el nombre de jugador y que se guarden en un Array (Clase jugador)
+//        Que eso este fuera del while, el menu siempre se tiene que mostrar
     }
 }
